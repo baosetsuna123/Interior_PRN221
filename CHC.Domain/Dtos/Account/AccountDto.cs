6fea6fa98e87cf32e31225dd28786b9e4e9af669
Setsuna <baohse321@gmail.com>
2024-03-21 08:35:54 +0700

    revert

--- CHC.Domain/Dtos/Account/AccountDto.cs
+++ CHC.Domain/Dtos/Account/AccountDto.cs
@@ -12,6 +12,7 @@ namespace CHC.Domain.Dtos.Account
 {
     public class AccountDto : BaseEntity
     {
+        public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
